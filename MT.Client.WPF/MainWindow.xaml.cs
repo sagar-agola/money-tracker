44634ce047f516a0cb6ab71db4dda4c072841ec1
Sagar Agola <N3o735@gmail.com>
2022-06-03 16:54:29 +0530

    style main window

--- MT.Client.WPF/MainWindow.xaml.cs
+++ MT.Client.WPF/MainWindow.xaml.cs
@@ -1,28 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace MT.Client.WPF
+namespace MT.Client.WPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+    }
+
+    private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
         {
-            InitializeComponent();
+            DragMove();
         }
+    }
+
+    private void MinimizeCommand(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void MaximizeCommand(object sender, RoutedEventArgs e)
+    {
+        WindowState ^= WindowState.Maximized;
+    }
+
+    private void CloseCommand(object sender, RoutedEventArgs e)
+    {
+        Close();
     }
 }
