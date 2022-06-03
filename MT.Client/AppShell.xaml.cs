c01fc3f6d8c3888f3edf6c8752a92249525dcb41
Sagar Agola <N3o735@gmail.com>
2022-06-03 16:10:12 +0530

    removed MAUI project

--- MT.Client/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MT.Client;
-
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
-    }
-}
--- MT.Client/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using Foundation;
-
-namespace MT.Client;
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
