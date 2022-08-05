48d26ccc2a0b6c510d4a3deb7619813f14cc73b2
Sagar Agola <35369309+sagar-agola@users.noreply.github.com>
2022-08-06 01:08:19 +0530

    added empty modules and setup routing

--- MT.Client.Web/src/app/app-routing.module.ts
+++ MT.Client.Web/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "transactions",
+    loadChildren: () => import("src/app/modules/transaction/transaction.module").then(m => m.TransactionModule)
+  },
+  {
+    path: "categories",
+    loadChildren: () => import("src/app/modules/category/category.module").then(m => m.CategoryModule)
+  },
+  {
+    path: "",
+    redirectTo: "transactions",
+    pathMatch: "full"
+  },
+  {
+    path: "**",
+    redirectTo: "transactions",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ MT.Client.Web/src/app/modules/transaction/transaction-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TransactionListComponent } from './transaction-list/transaction-list.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: TransactionListComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TransactionRoutingModule { }
